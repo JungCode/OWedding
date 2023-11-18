61d955dfb72be606ca5f979d466e9442c4577890
truong08112004 <truong08112004@gmail.com>
2023-11-18 19:14:46 +0700

    75% cua lol nhan dua cho han voc con thieu 1 modal

--- public/todolist/js.js
+++ public/todolist/js.js
@@ -18,3 +18,52 @@ function handleClickAccordition(e) {
     icon.classList.toggle("fa-angle-down");
     icon.classList.toggle("fa-angle-up");
 }
+
+///////////////check box
+const checkboxes = document.querySelectorAll('.checkbox');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', function () {
+            const span = this.parentElement.nextElementSibling.querySelector('.inneritem-text_main');
+            span.style.textDecoration = this.checked ? 'line-through' : 'none';
+        });
+    });
+
+
+//////////modal
+
+function addModalEvent(btnevent, modalclass) {
+  $(btnevent).click(function () {
+    $(modalclass).animate({"top": ($(document).innerHeight() / 2) - $(modalclass).height() + 30}, 300);
+    setTimeout(function () {
+      $(modalclass).animate({"top": ($(document).innerHeight() / 2) - $(modalclass).height()}, 300);
+    }, 300);
+    $(".modal-bg").fadeIn("fast");
+  });
+
+  $('.close, .modal-bg').click(function () {
+    $(modalclass).animate({"top": ($(document).innerHeight() / 2) - $(modalclass).height() + 30}, 300);
+    setTimeout(function () {
+      $(modalclass).animate({"top": -$(document).innerHeight()}, 300);
+    }, 300);
+    $(".modal-bg").fadeOut("fast");
+  });
+}
+
+addModalEvent('.btnaddfirst','.modaladdfirst');
+
+// $('.btnaddfirst').click( function() {
+//     $('.modal').animate({"top" : ($(document).innerHeight()/2) - $('.modal').height() + 30}, 300);
+//   setTimeout(function(){
+//     $('.modal').animate({"top" : ($(document).innerHeight()/2) - $('.modal').height()}, 300);
+//   },300);
+//     $(".modal-bg").fadeIn("fast");
+//   });
+  
+//   $('.close, .modal-bg').click( function() {
+//     $('.modal').animate({"top" : ($(document).innerHeight()/2) - $('.modal').height() + 30}, 300);
+//   setTimeout(function(){
+//     $('.modal').animate({"top" : -$(document).innerHeight()}, 300);
+//   },300);
+//     $(".modal-bg").fadeOut("fast");
+//   });
